d0247564ee512b46cc9a07d830fed1423aebd44f
Tafadzwa Kahwai <kahwaitafadzwa@gmail.com>
2023-10-16 22:13:28 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'; // Import your CSS file for styling
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [],
+    };
+  }
+
+  API_URL = "http://localhost:7298/";
+
+  componentDidMount() {
+    this.refreshNotes();
+  }
+
+  async addClick() {
+    var Newnotes = document.getElementById("Newnotes").value;
+    const data = new FormData();
+    data.append("Newnotes", Newnotes);
+
+    fetch(this.API_URL + "api/TodoApp/AddNotes", {
+      method: "POST",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        alert(result);
+        this.refreshNotes();
+      });
+  }
+
+  async deleteClick(id) {
+    try {
+      const response = await fetch(this.API_URL + "api/TodoApp/DeleteNotes?id=" + id, {
+        method: "DELETE",
+      });
+      const result = await response.json();
+      alert(result);
+      this.refreshNotes();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async refreshNotes() {
+    fetch(this.API_URL + 'api/TodoApp/GetNotes')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ notes: data });
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
+  }
+
+  render() {
+    const { notes } = this.state;
+    return (
+      <div className="App">
+        <div className="header">
+          <h2>My First React To-Do App</h2>
+          <input id="Newnotes" />
+          <button onClick={() => this.addClick()}>Add Notes</button>
+        </div>
+        <div className="notes">
+          {notes.map((note) => (
+            <div key={note.ID} className="note">
+              <p>
+                <b>*{note.ID}. {note.Description}</b>
+              </p>
+              <button onClick={() => this.deleteClick(note.ID)}>Delete Notes</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
